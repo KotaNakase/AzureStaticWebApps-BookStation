0f8f94b1a1a8bcbe6110b61e64bd601b71a2a095
KotaNakase <nakko1130@gmail.com>
2026-04-16 21:45:35 +0900

    ルートコンポーネント修正

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from "./router";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle";
 // import b-table
 import BootstrapVue from "bootstrap-vue";
+
+// import fortawesome(for https://startbootstrap.com/ template this is licenced by MIT)
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "datatables.net-bs4/css/dataTables.bootstrap4.css";
+import "../public/css/sb-admin.min.css";
 
 // import JQuery(for Bootstrap etc)
 import jQuery from "jquery";
