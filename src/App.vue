562cf8869fdf2be67b1942dff78618ea775404f0
KotaNakase <nakko1130@gmail.com>
2026-04-16 21:11:26 +0900

    ルートコンポーネント修正

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,25 @@
 <template>
   <div>
-    <div>
-      <b-table striped responsive hover :items="items" :fields="fields" />
-    </div>
-    <button class="btn-primary btn-sm" v-on:click="signIn()">test</button>
+    <button class="btn-primary btn-sm" v-on:click="goToSignIn()">サインインへ</button>
+    <!-- ルーターによって各ページコンポーネントがここに描画される -->
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  data() {
-    return {
-      items: [],
-      body: "",
-      fields: [
-        { key: "id", label: "ID" },
-        { key: "title", label: "タイトル" },
-        { key: "content", label: "内容" },
-      ],
-    };
-  },
-  async mounted() {
-    const response = await fetch("/api/dataaccess");
-    this.body = await response.json();
-    for (let item of this.body) {
-      this.items.push({
-        id: item.ID,
-        title: item.Title,
-        content: item.Content,
-      });
-    }
-  },
-    methods: {
+  methods: {
     /**
-     * サインイン処理
+     * サインインページへ遷移
      */
-    async signIn() {
-      this.$router.push({ name: "top" }).catch(err => {
-      // NavigationDuplicated エラーのみ無視する
-      if (err.name !== 'NavigationDuplicated') {
-        throw err;
-       }
-    });
-    //   this.msg = "";
-    //   this.errMsg = "";
-
-    //   this.isLoading = true;
-
-    //   try {
-    //     await UserUtil.signIn(this.userId, this.password);
-    //     this.$router.push({ name: "top" });
-    //   } catch (e) {
-    //     if (e.response.status === 401) {
-    //       this.errMsg = "サインインに失敗しました";
-    //     } else {
-    //       this.errMsg = e.message;
-    //     }
-    //   } finally {
-    //     this.isLoading = false;
-    //   }
+    goToSignIn() {
+      this.$router.push({ name: "signIn" }).catch(err => {
+        // NavigationDuplicated エラーのみ無視する
+        if (err.name !== "NavigationDuplicated") {
+          throw err;
+        }
+      });
     },
   },
 };
